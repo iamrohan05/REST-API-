4f01a5d71199fab3d6e97cbba97857a282e762e1
Rohan Bhandari <rohanbhandari124@gmail.com>
2025-06-27 23:27:27 -0500

    API Basics

--- practice.py
+++ practice.py
@@ -1,10 +1,30 @@
-from flask import Flask
-app= Flask(__name__)
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/get-user/<user_id>')
+def get_user(user_id):
+    user_data = {
+        "user_id": user_id,
+        "user_name": "Luffy",
+        "email": "iamluffy55@email.com"
+    }
+
+    extra = request.args.get("extra")
+
+    if extra:
+        user_data["extra"] = extra
+
+    return jsonify(user_data), 200
+
 @app.route('/')
+def home():
+    return "home"
 
-def index():
-    return 'Hello!'
+@app.route("/create-user/", methods=["POST"])
+def create_user():
+    data = request.get_json()
+    return jsonify(data), 201
 
-@app.route('/drinks')
-def get_drinks():
-    return {"drinks":"drink data"}
+if __name__ == "__main__":
+    app.run(debug=True)
